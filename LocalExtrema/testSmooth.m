3c46b2f9a950b8fcf85ab34bac82a7d1b2fee93e
Shuen-Huei Guan <drake.guan@gmail.com>
2011-11-06 15:58:17 +0800

    LocalExtrema: Change to use a cell array of strings for input filenames; and apply ABS on detail layer for better visualization.

--- LocalExtrema/testSmooth.m
+++ LocalExtrema/testSmooth.m
@@ -1,11 +1,11 @@
-I = imread('../images/flower.jpg');
-M = localExtrema(I, 7);
+images = {'../images/flower.jpg', '../images/rock2.jpg'};
+for i=1:size(images, 2)
+    filename = images{1, i};
+    I = double(imread(filename))/255;
+    M = localExtrema(I, 7);
+    D = I - M;
 
-figure; imshow(I); title('original image: flower');
-figure; imshow(M); title('smoothed image: flower');
-
-I = imread('../images/rock2.jpg');
-M = localExtrema(I, 7);
-
-figure; imshow(I); title('original image: rock2');
-figure; imshow(M); title('smoothed image: rock2');
+    figure; imshow(I); title(['original image: ' filename]);
+    figure; imshow(M); title(['smoothed image: ' filename]);
+    figure; imshow(abs(D)); title(['detail layer: ' filename]);
+end
