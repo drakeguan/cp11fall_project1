67637a582bf29b481bf1cd4e7ee20268bbfd615f
Shuen-Huei Guan <drake.guan@gmail.com>
2011-11-06 15:35:25 +0800

    LocalExtrema: Add one more testing image for smoothing.

--- LocalExtrema/testSmooth.m
+++ LocalExtrema/testSmooth.m
@@ -1,5 +1,11 @@
-im = imread('../images/flower.jpg');
-output = localExtrema(im);
+I = imread('../images/flower.jpg');
+M = localExtrema(I, 7);
 
-figure; imshow(im); title('original image');
-figure; imshow(output); title('smoothed image');
+figure; imshow(I); title('original image: flower');
+figure; imshow(M); title('smoothed image: flower');
+
+I = imread('../images/rock2.jpg');
+M = localExtrema(I, 7);
+
+figure; imshow(I); title('original image: rock2');
+figure; imshow(M); title('smoothed image: rock2');
