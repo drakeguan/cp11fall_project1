d83920d3380e85161390962a6710e02e84827394
Shuen-Huei Guan <drake.guan@gmail.com>
2011-11-08 15:55:16 +0800

    First version of workable cross-filters (just 3 now) testSmooth.

--- testSmooth.m
+++ testSmooth.m
@@ -1,8 +1,18 @@
 
-inputPath = '../images/';
-outputPath = '../result/';
+inputPath = 'images/';
+outputPath = 'result/';
 
+filters = {
+    'wlsFilter' 'Y';
+    'bilateralFilter' 'Y, [], 0, 1, 10, 0.2';
+    'localExtrema' 'Y, 11'
+};
 images = {[inputPath 'flower.jpg'], [inputPath 'rock2.jpg']};
+
+% update/add path
+for j=1:size(filters(:, 1))
+    addpath(filters{j, 1});
+end
 
 for i=1:size(images, 2)
     filename = images{1, i};
@@ -10,14 +20,18 @@ for i=1:size(images, 2)
     Y = double(rgb2gray(I))/255;
     [height, width, channel] = size(I);
 
-    M = wlsFilter(Y);
-    D = Y - M;
+    for j=1:size(filters(:, 1))
+        expression = sprintf('%s(%s)', filters{j, 1}, filters{j, 2});
+        % ex, localExtrema(Y, 3)
+        M = eval(expression);
+        D = Y - M;
 
-    figure; imshow(Y); title(['original image: ' filename]);
-    figure; imshow(M); title(['smoothed image: ' filename]);
-    figure; imshow(D+0.5); title(['detail layer: ' filename]);
+        figure; imshow(Y); title(['original image: ' filename]);
+        figure; imshow(M); title(['smoothed image: ' filename]);
+        figure; imshow(D+0.5); title(['detail layer: ' filename]);
 
-    row = floor(height/3);
-    figure; plot(1:width, Y(row,:,1), 1:width, M(row,:,1), 1:width, D(row,:,1), 'LineWidth', 2);
-    title(['plot of smoothed result of' filename ' by I, M and D']);
+        row = floor(height/3);
+        figure; plot(1:width, Y(row,:), 1:width, M(row,:), 1:width, D(row,:), 'LineWidth', 2);
+        title(['plot of smoothed result of' filename ' by Y, M and D']);
+    end
 end
